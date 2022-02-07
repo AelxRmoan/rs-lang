ee4472dde1d4cc3b35aeeb8960e6e64cc5a19cc2
AlexRoman <mirmaker181@gmail.com>
2022-02-07 14:55:15 +0200

    feat: adjust header styles

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,3 +1,4 @@
+import css from './button.css'
 import { Button as MuiButton } from '@mui/material';
 
 interface Props {
@@ -5,9 +6,9 @@ interface Props {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const Button: React.FC<Props> = ({ children, selector, onClick}) => {
+export const Button: React.FC<Props> = ({children, selector, onClick}) => {
   return (
-    <MuiButton variant="contained" className={selector} onClick={onClick}>
+    <MuiButton variant="contained" className={`${css.btn} ${selector}`} onClick={onClick}>
       {children}
     </MuiButton>
   );
